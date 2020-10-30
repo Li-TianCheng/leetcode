0c6573029033398b24d3fcc02599510142261b7d
Li-TianCheng <li_tiancheng666@163.com>
2020-10-30 16:08:48 +0800

    update

---
+++ c++/[99]恢复二叉搜索树.cpp
@@ -0,0 +1,93 @@
+//二叉搜索树中的两个节点被错误地交换。 
+//
+// 请在不改变其结构的情况下，恢复这棵树。 
+//
+// 示例 1: 
+//
+// 输入: [1,3,null,null,2]
+//
+//   1
+//  /
+// 3
+//  \
+//   2
+//
+//输出: [3,1,null,null,2]
+//
+//   3
+//  /
+// 1
+//  \
+//   2
+// 
+//
+// 示例 2: 
+//
+// 输入: [3,1,4,null,null,2]
+//
+//  3
+// / \
+//1   4
+//   /
+//  2
+//
+//输出: [2,1,4,null,null,3]
+//
+//  2
+// / \
+//1   4
+//   /
+//  3 
+//
+// 进阶: 
+//
+// 
+// 使用 O(n) 空间复杂度的解法很容易实现。 
+// 你能想出一个只使用常数空间的解决方案吗？ 
+// 
+// Related Topics 树 深度优先搜索 
+// 👍 361 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* prev = nullptr;
+    TreeNode* x = nullptr;
+    TreeNode* y = nullptr;
+    void recoverTree(TreeNode* root) {
+        BFS(root);
+        int temp = x->val;
+        x->val = y->val;
+        y->val = temp;
+    }
+
+    void BFS(TreeNode* node){
+        if (node == nullptr){
+            return;
+        }
+        BFS(node->left);
+        if (prev != nullptr && node->val <= prev->val){
+            y = node;
+            if (x == nullptr){
+                x = prev;
+            }else{
+                return;
+            }
+        }
+        prev = node;
+        BFS(node->right);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
