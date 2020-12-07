41c185a07db619d2135b03a23cac77b53c1f9f66
Li-TianCheng <li_tiancheng666@163.com>
2020-12-07 12:48:04 +0800

    update

---
+++ c++/[173]二叉搜索树迭代器.cpp
@@ -0,0 +1,80 @@
+//实现一个二叉搜索树迭代器。你将使用二叉搜索树的根节点初始化迭代器。 
+//
+// 调用 next() 将返回二叉搜索树中的下一个最小的数。 
+//
+// 
+//
+// 示例： 
+//
+// 
+//
+// BSTIterator iterator = new BSTIterator(root);
+//iterator.next();    // 返回 3
+//iterator.next();    // 返回 7
+//iterator.hasNext(); // 返回 true
+//iterator.next();    // 返回 9
+//iterator.hasNext(); // 返回 true
+//iterator.next();    // 返回 15
+//iterator.hasNext(); // 返回 true
+//iterator.next();    // 返回 20
+//iterator.hasNext(); // 返回 false 
+//
+// 
+//
+// 提示： 
+//
+// 
+// next() 和 hasNext() 操作的时间复杂度是 O(1)，并使用 O(h) 内存，其中 h 是树的高度。 
+// 你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 中至少存在一个下一个最小的数。 
+// 
+// Related Topics 栈 树 设计 
+// 👍 291 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+public:
+    stack<TreeNode*> stack;
+
+    BSTIterator(TreeNode* root) {
+        while (root != nullptr){
+            stack.push(root);
+            root = root->left;
+        }
+    }
+    
+    int next() {
+        TreeNode* node = stack.top();
+        stack.pop();
+        int val = node->val;
+        node = node->right;
+        while (node != nullptr){
+            stack.push(node);
+            node = node->left;
+        }
+        return val;
+    }
+    
+    bool hasNext() {
+        return !stack.empty();
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
