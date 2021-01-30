1d82a445c48287d1ea7c9a327bf40f477a4d5518
Li-TianCheng <li_tiancheng666@163.com>
2021-01-30 13:29:47 +0800

    update

---
+++ c++/[329]矩阵中的最长递增路径.cpp
@@ -0,0 +1,90 @@
+//给定一个 m x n 整数矩阵 matrix ，找出其中 最长递增路径 的长度。 
+//
+// 对于每个单元格，你可以往上，下，左，右四个方向移动。 你 不能 在 对角线 方向上移动或移动到 边界外（即不允许环绕）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：matrix = [[9,9,4],[6,6,8],[2,1,1]]
+//输出：4 
+//解释：最长递增路径为 [1, 2, 6, 9]。 
+//
+// 示例 2： 
+//
+// 
+//输入：matrix = [[3,4,5],[3,2,6],[2,2,1]]
+//输出：4 
+//解释：最长递增路径是 [3, 4, 5, 6]。注意不允许在对角线方向上移动。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：matrix = [[1]]
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == matrix.length 
+// n == matrix[i].length 
+// 1 <= m, n <= 200 
+// 0 <= matrix[i][j] <= 231 - 1 
+// 
+// Related Topics 深度优先搜索 拓扑排序 记忆化 
+// 👍 417 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        vector<vector<int>> cache(matrix.size(), vector<int>(matrix[0].size(), -1));
+        int result = 0;
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                result = max(result, dfs(matrix, cache, i, j));
+            }
+        }
+        return result;
+    }
+
+    int dfs(vector<vector<int>>& matrix, vector<vector<int>>& cache, int x, int y){
+        if (cache[x][y] != -1){
+            return cache[x][y];
+        }
+        int curr = 1;
+        if (x != 0 && matrix[x-1][y] > matrix[x][y]){
+            if (cache[x-1][y] == -1){
+                cache[x-1][y] = dfs(matrix, cache, x-1, y);
+            }
+            curr = max(curr, cache[x-1][y]+1);
+        }
+        if (x != matrix.size()-1 && matrix[x+1][y] > matrix[x][y]){
+            if (cache[x+1][y] == -1){
+                cache[x+1][y] = dfs(matrix, cache, x+1, y);
+            }
+            curr = max(curr, cache[x+1][y]+1);
+        }
+        if (y != 0 && matrix[x][y-1] > matrix[x][y]){
+            if (cache[x][y-1] == -1){
+                cache[x][y-1] = dfs(matrix, cache, x, y-1);
+            }
+            curr = max(curr, cache[x][y-1]+1);
+        }
+        if (y != matrix[0].size()-1 && matrix[x][y+1] > matrix[x][y]){
+            if (cache[x][y+1] == -1){
+                cache[x][y+1] = dfs(matrix, cache, x, y+1);
+            }
+            curr = max(curr, cache[x][y+1]+1);
+        }
+        cache[x][y] = curr;
+        return curr;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
